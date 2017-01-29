f175037dd1cc1c1ac4cd842264645f3b96704b6e
Javier Gamarra <nhpatt@gmail.com>
2017-01-29 07:16:03 -0800

    rename icons

--- app/src/main/java/com/liferay/home/liferayhome/services/GeofenceTransitionsIntentService.java
+++ app/src/main/java/com/liferay/home/liferayhome/services/GeofenceTransitionsIntentService.java
@@ -14,6 +14,10 @@ import com.liferay.home.liferayhome.HomeActivity;
 import com.liferay.home.liferayhome.R;
 import java.util.List;
 
+import static com.google.android.gms.location.Geofence.GEOFENCE_TRANSITION_ENTER;
+import static com.google.android.gms.location.Geofence.GEOFENCE_TRANSITION_EXIT;
+import static com.liferay.home.liferayhome.LiferayHomeActivity.TAG;
+
 public class GeofenceTransitionsIntentService extends IntentService {
 
 	public GeofenceTransitionsIntentService() {
@@ -21,51 +25,41 @@ public class GeofenceTransitionsIntentService extends IntentService {
 	}
 
 	protected void onHandleIntent(Intent intent) {
-		Log.e("LiferayHome", "Event!");
+
+		Log.d(TAG, "Received a geofencing event!");
 		GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
 		if (geofencingEvent.hasError()) {
-
-			//FIXME get the error
 			Log.e("LiferayHome", String.valueOf(geofencingEvent.getErrorCode()));
 			return;
 		}
 
-		int geofenceTransition = geofencingEvent.getGeofenceTransition();
-		if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER
-			|| geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+		int transition = geofencingEvent.getGeofenceTransition();
+		if (transition == GEOFENCE_TRANSITION_ENTER || transition == GEOFENCE_TRANSITION_EXIT) {
 
 			List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+			Geofence geofence = triggeringGeofences.get(0);
+			Log.d(TAG, geofence.toString());
 
-			Log.e("LiferayHome", "Event!" + triggeringGeofences.get(0).toString());
-
-			createNotification(triggeringGeofences,
-				geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ? "close to" : "leaving");
-			Log.i(HomeActivity.TAG, triggeringGeofences.toString());
-		} else {
-			Log.e(HomeActivity.TAG, "Error!");
+			String message = transition == GEOFENCE_TRANSITION_ENTER ? "We're close to home" : "We're leaving home";
+			createNotification(triggeringGeofences, message);
 		}
 	}
 
 	private void createNotification(List<Geofence> triggeringGeofences, String message) {
-		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this).setSmallIcon(R.drawable.ic_home_2)
-			.setContentTitle(triggeringGeofences.get(0).getRequestId())
-			.setContentText("We're " + message + " home!");
-		// Creates an explicit intent for an Activity in your app
-		Intent resultIntent = new Intent(this, HomeActivity.class);
 
-		// The stack builder object will contain an artificial back stack for the
-		// started Activity.
-		// This ensures that navigating backward from the Activity leads out of
-		// your application to the Home screen.
+		NotificationCompat.Builder notificationBuilder =
+			new NotificationCompat.Builder(this).setSmallIcon(R.drawable.home_icon)
+				.setContentTitle(triggeringGeofences.get(0).getRequestId())
+				.setContentText(message);
+
+		Intent resultIntent = new Intent(this, HomeActivity.class);
 		TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-		// Adds the back stack for the Intent (but not the Intent itself)
 		stackBuilder.addParentStack(HomeActivity.class);
-		// Adds the Intent that starts the Activity to the top of the stack
 		stackBuilder.addNextIntent(resultIntent);
+
 		PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-		mBuilder.setContentIntent(resultPendingIntent);
-		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		// mId allows you to update the notification later on.
-		mNotificationManager.notify(0, mBuilder.build());
+		notificationBuilder.setContentIntent(resultPendingIntent);
+		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.notify(0, notificationBuilder.build());
 	}
 }
